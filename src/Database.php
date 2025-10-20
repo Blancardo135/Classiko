a0266afe471c7e75762fc68ed7a0808e5fa66c52
Raul Gomez <raul.gomezcalvo@heig-vd.ch>
2025-10-20 12:23:24 +0200

    Database.php Modifs

--- src/Database.php
+++ src/Database.php
@@ -1,19 +1,41 @@
 <?php
+
 class Database {
+
+    const DATABASE_FILE = '../teamsmanager.db';
+
     private $pdo;
 
     public function __construct() {
-        $this->connect();
+        $this->pdo = new PDO("sqlite:" . self::DATABASE_FILE);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->createTables();
     }
 
-    private function connect() {
-        try {
-            $this->pdo = new PDO("sqlite:" . __DIR__ . "/../teamsmanager.db");
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            die("Erreur de connexion à la base de données : " . $e->getMessage());
-        }
+    private function createTables() {
+
+        $sqlTeams = "CREATE TABLE IF NOT EXISTS teams (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            nbPlayers INTEGER NOT NULL,
+            descr TEXT,
+            sport TEXT NOT NULL
+        );";
+
+        $sqlPlayers = "CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT,
+            club TEXT,
+            position TEXT,
+            team_id INTEGER,
+            FOREIGN KEY (team_id) REFERENCES teams(id)
+        );";
+
+        $this->pdo->exec($sqlTeams);
+        $this->pdo->exec($sqlPlayers);
     }
 
     public function getPdo() {
