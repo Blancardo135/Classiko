b2312fdb5c5c6f202e346db28c5378e9391058c2
Blancardo135 <blanchardromain02@gmail.com>
2025-10-25 13:51:20 +0200

    Modifications pour database.ini

---
+++ Database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database {
+
+    const DATABASE_FILE = '../teamsmanager.db';
+
+    private $pdo;
+
+    public function __construct() {
+        $this->pdo = new PDO("sqlite:" . self::DATABASE_FILE);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->createTables();
+    }
+
+    private function createTables() {
+
+        $sqlTeams = "CREATE TABLE IF NOT EXISTS teams (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            nbPlayers INTEGER NOT NULL,
+            descr TEXT,
+            sport TEXT NOT NULL
+        );";
+
+        $sqlPlayers = "CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT,
+            club TEXT,
+            position TEXT,
+            team_id INTEGER,
+            FOREIGN KEY (team_id) REFERENCES teams(id)
+        );";
+
+        $this->pdo->exec($sqlTeams);
+        $this->pdo->exec($sqlPlayers);
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+}
+//V Finale, quelques problèmes concernant la db, les tables ne s'affichent pas mais sont bien là ;)
