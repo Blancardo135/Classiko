8e52d154d7ad7c4ccd501f30c47420f8bdbc3643
Blancardo135 <blanchardromain02@gmail.com>
2025-11-21 16:08:21 +0100

    Problème require

--- src/classes/Mail.php
+++ src/classes/Mail.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../utils/autoloader.php';
+require_once __DIR__ . '/../utils/autoloader.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
